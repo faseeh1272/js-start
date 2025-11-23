cd11b12ceb65968bc6a7ab1f190edea646c96893
Faseeh Hussain <syedfaseehnaqvi2@gmail.com>
2025-11-24 03:43:13 +0500

    js start

---
+++ app.js
@@ -0,0 +1,87 @@
+// alert("this is an alert message! 111111111");
+
+
+// var my name = 25 space not allowed
+
+// var _myname = 25 _ and $ sign allowed just
+
+// var 1name = 25 nuber at start not allowed
+
+// var myName1 = 25 Number between and end allowed
+
+// camel case
+
+// var myName = 25 
+
+
+// var NyName =25 THIS IS A pascal case
+
+// snack case 
+
+// var my_first_name = 25 this is a snack case
+
+// var  MY_NAME_NAME = 25 THIS A SCREAMING CASE 
+
+// DATA TYPES 
+
+
+// primitive
+
+// 1: string
+let profile ; 
+console.log(profile );
+// let data = null;
+// console.log(data);   // null
+
+let fasih = null;
+console.log(fasih);
+
+
+let ibrahim;
+console.log(ibrahim)
+
+let nomi = null;
+
+console.log(nomi);
+
+
+let big =  123456787644543468877653588549n;
+console.log(big);
+
+const id = Symbol("myid");
+
+console.log(id);
+
+let no = "a" + 2;
+
+console.log(no);
+
+
+
+
+
+
+// let name = "faseeh";
+
+// 2 : number
+
+// let age = 25;
+
+// let price = 2.70;
+
+// 3: Boolean (true and false)
+
+
+// let isOnline = true;
+// let isLoggedIn = false;
+
+// console.log("")
+
+
+
+
+
+
+
+
+
